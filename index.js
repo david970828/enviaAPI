fe9fc4c971b2777db74f6d026b67d9127f7b49c5
David Ricardo Pardo <david970828@gmail.com>
2021-09-17 00:13:36 -0500

    fix si

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-/**
- * Configuraciones de servidor
- */
+import http from 'http'
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { api } from "./api";
 
-require('@babel/register')({
-  presets: ['@babel/preset-env'],
+const app = express()
+
+app.use(cors())
+app.use(express.json({ limit: '200MB' }));
+app.use(
+  express.urlencoded({ extended: true, limit: '200MB', parameterLimit: 50000 })
+);
+app.use(cookieParser());
+
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('RestFull API ENVIA started on: ' + port);
 });
 
-require('./src/server');
+app.get('/', (req, res) => {
+  res.send('Hola mundo')
+})
+
+app.use('/api',api)
