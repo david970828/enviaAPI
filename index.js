bd2ecac312e9617aac7251f083ab958e64af7852
david.ramirez <cdavidramirezg24@gmail.com>
2021-09-17 00:49:23 -0500

    now api

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-var sessionStorage = require('sessionstorage');
-var bodyParser = require('body-parser');
-var express = require('express'),
-app = express(),
-port = process.env.PORT || 3000;
-
-
-
-app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  next();
-});
-app.listen(port);
-console.log('RestFull API QUASAR started on: ' + port);
-
-
-/************************************************************************************
- *                                API POST TOP SECRET                               *
- *    Obtiene la posicion y mensaje de la nave, a partir del payload de entrada,    *
- *    si no es posible obtenerlos responde un codigo HTTP 404                       *
- ************************************************************************************/
-app.post('/distribution',function(request, response) {
-    response.status(200).send({
-      position: {
-        x: 'coordinates[0]',
-        y: 'coordinates[1]'
-      },
-      message:'HOLA MUNDO'
-    });
-});
-
 
 /************************************************************************************
  *                           API GET TOP SECRET SPLIT                               *
