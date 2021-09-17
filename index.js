3201404906ff2f7ce3c2e4d1cd87c83a740266e4
david.ramirez <cdavidramirezg24@gmail.com>
2021-09-17 00:52:50 -0500

    server conf

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
+  
+/**
+ * Configuraciones de servidor
+ */
 
-/************************************************************************************
- *                           API GET TOP SECRET SPLIT                               *
- *    Permite obtener la posicion y mensaje de la nave a partir de minimo 3         *
- *    satelites almacenados en el sessionStorage, de no tener la cantidad de        *
- *    satelites, generara un codigo HTTP 404 - no hay informacion suficiente        *
- ************************************************************************************/
-app.get('/distribution',function(request, response) {
-  response.status(200).send({
-      message:'GET MESSAGE'
-    });
+require('@babel/register')({
+  presets: ['@babel/preset-env'],
 });
+
+require('./src/server');
