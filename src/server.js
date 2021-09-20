d676d798866d2b78b1baf1414e72f4ea8ca4b4ad
david.ramirez <cdavidramirezg24@gmail.com>
2021-09-20 08:44:55 -0500

    dotenv

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { api } from './api';
-import dotenv from 'dotenv';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv');
+  dotenv.config();
+}
 const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 5000;
